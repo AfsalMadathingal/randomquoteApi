f42e58e2213515e5167ef0a4d9a3d6645ba10852
afsal <madathimgalafsal@gmail.com>
2024-03-08 18:53:29 +0530

    update

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ require('dotenv').config()
 const quotesApi= require('./quotesApi.json');
 const port= process.env.PORT || 3000;
 
+
 app.get('/random-movie-quote', (req, res) => {
    
     const randomIndex = Math.floor(Math.random() * quotesApi.length);
     const randomQuote = quotesApi[randomIndex];
-    res.json(randomQuote);
+    console.log(randomQuote);
+    res.status(200).json(randomQuote);
   });
 
-  
+
 
 app.listen(port, () => {
     console.log(`api app listening at http://localhost:${port}`)
